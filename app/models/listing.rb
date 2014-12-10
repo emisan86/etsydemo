49cd86881d304c397ee48e686ad72893477c0410
Emilio Sanchez <emilio_sanchez03@hotmail.com>
2014-12-10 10:42:04 +0800

    First commit

---
+++ app/models/listing.rb
@@ -0,0 +1,5 @@
+class Listing < ActiveRecord::Base
+	has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "defaultimage.png"
+  	#do_not_validate_attachment_file_type :image
+  	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+end
